480a26361c9c6c8cff5053e727ffaf01b7b7e826
Alexis Suard <asuard@ecocea.com>
2018-08-22 14:18:14 +0200

    Add cellConfiguration in pod

---
+++ Podyfied/SnowGlobe/Pod/Classes/CellConfiguration.swift
@@ -0,0 +1,19 @@
+//
+//  CellConfiguration.swift
+//  Pods-SnowGlobe_Example
+//
+//  Created by Alexis Suard on 22/08/2018.
+//
+
+import Foundation
+
+public struct CellConfiguration {
+    public var birthRate: Float = 60
+    public var lifetime: Float = 25
+    public var scale: CGFloat = 0.2
+    public var scaleRange: CGFloat = 0.1
+    public var spin: CGFloat = 2
+    public var spinRange: CGFloat = 2
+    public var velocity: CGFloat = -150
+    public var velocityRange: CGFloat = -70.0
+}
